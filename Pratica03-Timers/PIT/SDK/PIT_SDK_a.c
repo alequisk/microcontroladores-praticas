49691f65321eef69ddc2c01d7693385632ba249e
Alex Sousa <alexsousa1435@gmail.com>
2022-05-31 10:45:26 -0300

    refactor: new struct folder

---
+++ Pratica03-Timers/PIT/SDK/PIT_SDK_a.c
@@ -0,0 +1,99 @@
+/**
+ * @file    PIT_SDK.c
+ * @brief   Application entry point.
+ */
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MKL43Z4.h"
+#include "fsl_debug_console.h"
+#include "fsl_pit.h"
+#include "fsl_gpio.h"
+#include "fsl_port.h"
+
+/*
+ * Timer in N sec cnt = (CLOCK_GetBusClkFreq) x N
+ *
+ * Timer in N mSec cnt = (CLOCK_GetBusClkFreq / 1000) X N
+ *
+ * Timer in N uSec cnt = (CLOCK_GetBusClkFreq / 1000000) x N
+ */
+
+#define GET_SEC_COUNT(x) (CLOCK_GetBusClkFreq() * x)
+#define GET_USEC_COUNT(x) ((CLOCK_GetBusClkFreq() * x)/1000000)
+uint32_t timerCnt;
+
+void PIT_IRQHandler(void) {
+	GPIO_PortToggle(GPIOE, (1<<31));
+	PIT_ClearStatusFlags(PIT, kPIT_Chnl_0, kPIT_TimerFlag);
+}
+
+void DelaySec(uint32_t sec)
+{
+	timerCnt=0;
+	PIT_StartTimer(PIT, kPIT_Chnl_0);
+	while((sec * 100000) > timerCnt);
+	PIT_StopTimer(PIT, kPIT_Chnl_0);
+}
+
+void DelayMsec(uint32_t mSec)
+{
+	timerCnt=0;
+	PIT_StartTimer(PIT, kPIT_Chnl_0);
+	while((mSec * 100) > timerCnt);
+	PIT_StopTimer(PIT, kPIT_Chnl_0);
+
+}
+
+void DelayUSec(uint32_t uSec)
+{
+	timerCnt=0;
+	PIT_StartTimer(PIT, kPIT_Chnl_0);
+	while((uSec / 10) > timerCnt);
+	PIT_StopTimer(PIT, kPIT_Chnl_0);
+}
+
+/*
+ * @brief   Application entry point.
+ */
+int main(void) {
+	pit_config_t pitCfg = {0};
+	gpio_pin_config_t gpioLed = {
+			kGPIO_DigitalOutput,
+			1
+	};
+
+	// Ativar o clock da PORTA_A e PORTA_C
+	CLOCK_EnableClock(kCLOCK_PortE);
+
+	// Ativar o GPIO da PORTA_C_9
+	PORT_SetPinMux(PORTE, 31, kPORT_MuxAsGpio);
+
+	// Configurar GPIO_C pino 9 como saída
+	GPIO_PinInit(GPIOE, 31, &gpioLed);
+
+	// Obter configuração padrão do PIT
+	PIT_GetDefaultConfig(&pitCfg);
+
+	// Initializar o PIT
+	PIT_Init(PIT, &pitCfg);
+
+	// Definir o período de tempo do PIT
+	PIT_SetTimerPeriod(PIT, kPIT_Chnl_0, GET_SEC_COUNT(1));
+
+	// Ativar o Timer de interrupção
+	PIT_EnableInterrupts(PIT, kPIT_Chnl_0, kPIT_TimerInterruptEnable);
+
+	// Ativar interrução do PIT no NVIC
+	NVIC_EnableIRQ(PIT_IRQn);
+
+	// Iniciar o Timer do PIT
+	PIT_StartTimer(PIT, kPIT_Chnl_0);
+
+	while(1) {
+
+	}
+    return 0 ;
+}
--- Pratica4-PWM/PWM_dutycycle.c
+++ Pratica4-PWM/PWM_dutycycle.c
@@ -10,7 +10,7 @@ typedef struct{
 	uint32_t PCR[32];
 }PORTRegs_t;
 
-
+#define PORT_A ((PORTRegs_t *)0x40049000)
 #define PORT_B ((PORTRegs_t *)0x4004A000)
 
 typedef struct{
@@ -37,35 +37,42 @@ typedef struct{
 
 #define SOPT2_REG ((SOPT2R_Regs_t *) 0x40048004)
 
+
+
 #define PTB0_PIN 0
 #define PTB1_PIN 1
+#define PTB2_PIN 2
+#define PTA12_PIN 12
 //#define FREQ_2_MOD(x) (375000/x)
 
-
+/*
+ * Configuração do canal 1 do módulo TPM1 com a função "PWM".
+ *
+ */
 void InitTPMPWM(void){
 	/*
 	 * 	System Clock Gating Control Register 5 (SIM_SCGC5)
 	 *| 31  --  20| 19 | 18 -- 14 | 13 | 12 | 11 | 10 |  9 | 8  7 | 6 |   5 | 4  3  2 | 1 |   0   |
 	 *|     0	  |  0 |     0    | PE | PD | PC | PB | PA |   1  | 0 | TSI	|    0    | 0 | LPTMR |
 	 */
-	SIM->SCGC5 = (1 << 10); //ativar clock porta B
+	SIM->SCGC5 = (1 << 10) | (1 << 9); //ativar clock porta B // mexi aqui
 	/*
 	 * 	Pin Control Register n (PORTx_PCRn)
 	 *| 31  --  25|  24 | 23 -- 20 | 19 -- 16 | 15 -- 11 | 10  9  8 | 7 |  6  | 5 |  4  | 3 |  2  | 1  |  0 |
 	 *|     0	  | ISF |     0    |   IRQC   |     0    |    MUX   | 0 | DSE | 0 | PFE | 0 | SRE | PE | PS |      | 0 | LPTMR |
 	 *|           | w1c |		   |
 	 */
-	PORT_B->PCR[PTB0_PIN] = 	(1 << 24) |		// ISF=PORTB_PCR18[24]: w1c (limpa a pendência)
+	PORT_B->PCR[PTB2_PIN] = 	(1 << 24) |		// ISF=PORTB_PCR18[24]: w1c (limpa a pendência)
 							(0b011 << 8);   // MUX=PORTB_PCR18[10:8]=0b011 (TPM2_CH0)
 
-//	PORT_B->PCR[PTB1_PIN] = 	(1 << 24) |		// ISF=PORTB_PCR18[24]: w1c (limpa a pendência)
-//								(0b011 << 8);   // MUX=PORTB_PCR18[10:8]=0b011 (TPM2_CH0)
+	PORT_A->PCR[PTA12_PIN] = 	(1 << 24) |		// ISF=PORTB_PCR18[24]: w1c (limpa a pendência)
+								(0b011 << 8);   // MUX=PORTB_PCR18[10:8]=0b011 (TPM2_CH0) // mexi aqui
 	/*
 	 * 	System Clock Gating Control Register 6 (SIM_SCGC6)
 	 *|   31 | 30 |  29 | 28 |  27  |  26  |  25  |  24  |  23 | 22 -- 2 |    1   |  0  |
 	 *| DAC0 |  0 | RTC |  0 | ADC0 | TPM2 | TPM1 | TPM0 | PIT |    0    | DMAMUX | FTF |
 	 */
-	SIM->SCGC6 = (1 << 25);
+	SIM->SCGC6 = (1 << 26) | (1 << 25); // mexi aqui
 
 	/*
 	 * 	System Options Register 2 (SIM_SOPT2)
@@ -73,7 +80,7 @@ void InitTPMPWM(void){
 	 *|      0     | UART0SRC | TPMSRC |    0     | USBSRC |  0 | PLLFLLSEL |     0   | CLKOUTSEL | RTCCLKOUTS |    0   |
 	 */
 	//PLL/FLL clock select
-	SOPT2_REG->sopt2 = (0 << 16);	//0 MCGFLLCLK clock
+	SOPT2_REG->sopt2 &= ~(1 << 16);	//0 MCGFLLCLK clock
 									//1 MCGPLLCLK clock with fixed divide by two
 	/*TPMSRC	 *
 	 * TPM clock source select
@@ -89,6 +96,7 @@ void InitTPMPWM(void){
 
 	//valor do modulo = 20971520 / 128 = 163840 / 3276 = 50 Hz
 	TPM1_REG->mod = 3276;    		// MOD=TPM2_MOD[15:0]=3276
+	TPM2_REG->mod = 3276;    		// MOD=TPM2_MOD[15:0]=3276
 
 	//valor do modulo = 48000000 / 128 = 375000 / 7500 = 50 Hz
 	//TPM1_REG->mod = 7500;    		// MOD=TPM2_MOD[15:0]=7500
@@ -99,101 +107,72 @@ void InitTPMPWM(void){
 	 *|      0    | DMA | TOF | TOIE | CPWMS | CMOD |   PS    |
 	 *|			  |     | w1c |
 	 */
+
+	/* LIXO
 	TPM1_REG->sc = 	(0 << 5)	|			// CPWMS=TPM2_SC[5]=0 (modo de contagem crescente)
-					(0b01 << 3)	|           // CMOD=TPM2_SC[4:3]=0b01 (incrementa a cada pulso do LPTPM)
+					(0b01 << 3)	|           // CMOD=TPM2PORTB_PCR18_SC[4:3]=0b01 (incrementa a cada pulso do LPTPM)
 					(0b111 << 0);           		// PS=TPM2_SC[2:0]=0b101 (Fator Prescale = 32)
+	*/
+
+	TPM2_REG->sc &= ~(1 << 5);
+	TPM2_REG->sc |= (0b01 << 3)	| (0b111 << 0);
+
+	TPM1_REG->sc &= ~(1 << 5);
+	TPM1_REG->sc |= (0b01 << 3)	| (0b111 << 0);
+
+	/* LIXO
+	TPM2_REG->sc = 	(0 << 5)	|			// CPWMS=TPM2_SC[5]=0 (modo de contagem crescente)
+						(0b01 << 3)	|           // CMOD=TPM2_SC[4:3]=0b01 (incrementa a cada pulso do LPTPM)
+						(0b111 << 0);           		// PS=TPM2_SC[2:0]=0b101 (Fator Prescale = 32)
+	*/
+
 
 	/* Desativar o PWM no modulo TPM1 canal 0 --> PTB0 */
-	TPM1_REG->CONTROLS[0].CnSC = (0 << 5) | // MSB =TPM2_C0SC[5]=0
-								 (0 << 4) | // MSA =TPM2_C0SC[4]=0
-								 (0 << 3) | // ELSB=TPM2_C0SC[3]=0
-								 (0 << 2);  // ELSA=TPM2_C0SC[2]=0
+	TPM1_REG->CONTROLS[0].CnSC &= ~(1 << 5) & // MSB =TPM2_C0SC[5]=0
+								 ~(1 << 4) & // MSA =TPM2_C0SC[4]=0
+								 ~(1 << 3) & // ELSB=TPM2_C0SC[3]=0
+								 ~(1 << 2);  // ELSA=TPM2_C0SC[2]=0
+
+	/* Desativar o PWM no modulo TPM1 canal 0 --> PTB0 */
+	TPM2_REG->CONTROLS[0].CnSC &= ~(1 << 5) & // MSB =TPM2_C0SC[5]=0
+								 ~(1 << 4) & // MSA =TPM2_C0SC[4]=0
+								 ~(1 << 3) & // ELSB=TPM2_C0SC[3]=0
+								 ~(1 << 2);  // ELSA=TPM2_C0SC[2]=0
+
 
 	/* Ativar o PWM no modulo TPM1 canal 0 --> PTB0 */
-	TPM1_REG->CONTROLS[0].CnSC = (1 << 5) | // MSB =TPM2_C0SC[5]=0
-								 (0 << 4) | // MSA =TPM2_C0SC[4]=0
-								 (1 << 3) | // ELSB=TPM2_C0SC[3]=0
-								 (0 << 2);  // ELSA=TPM2_C0SC[2]=0
+	TPM1_REG->CONTROLS[0].CnSC = ((1 << 5) | // MSB =TPM2_C0SC[5]=0
+								 (1 << 3)) &  // ELSB=TPM2_C0SC[3]=0
+								 ~((1 << 4) & // MSA =TPM2_C0SC[4]=0
+								 ~(1 << 2));  // ELSA=TPM2_C0SC[2]=0
+
+	/* Ativar o PWM no modulo TPM1 canal 0 --> PTB0 */
+	TPM2_REG->CONTROLS[0].CnSC = ((1 << 5) | // MSB =TPM2_C0SC[5]=0
+							 (1 << 3)) & // ELSB=TPM2_C0SC[3]=0
+							 ~((1 << 4) & // MSA =TPM2_C0SC[4]=0
+							 ~(1 << 2));  // ELSA=TPM2_C0SC[2]=0
 }
 /*
  * @brief   Application entry point.
  */
-
-
-#define ORIGEM_0    245
-#define DIREITA_90  345
-#define DIREITA_180 170
-
-
-void delay() {
-	for (int i = 0; i < 50000; i++);
-}
-
-void delay2();
-
-void setupBeforeChangeCnv();
-
-void steps() {
-	// Origem -> 90
-	// 90 -> origem
-	// origem -> -90
-	// 90 -> 0
-	setupBeforeChangeCnv();
-	for (int i = ORIGEM_0; i < DIREITA_90; i++) {
-		TPM1_REG->CONTROLS[0].CnV = i;
-		delay();
-	}
-	setupBeforeChangeCnv();
-	for (int i = DIREITA_90; i > DIREITA_180; i--) {
-		TPM1_REG->CONTROLS[0].CnV = i;
-		delay();
-	}
-
-	setupBeforeChangeCnv();
-	for (int i = DIREITA_180; i < ORIGEM_0; i++) {
-		TPM1_REG->CONTROLS[0].CnV = i;
-		delay();
-	}
-
-}
-
-void setupBeforeChangeCnv() {
-	TPM1_REG->CONTROLS[0].CnSC &= (1 << 5);   // MSB =TPM2_C0SC[5]=0
-	TPM1_REG->CONTROLS[0].CnSC &= ~(1 << 4);  // MSA =TPM2_C0SC[4]=0
-	TPM1_REG->CONTROLS[0].CnSC &= ~(1 << 3);  // ELSB=TPM2_C0SC[3]=0
-	TPM1_REG->CONTROLS[0].CnSC &= ~(1 << 2);  // ELSA=TPM2_C0SC[2]=0
-
-	TPM1_REG->CONTROLS[0].CnSC |= (1 << 5) | // MSB =TPM2_C0SC[5]=0
-								 (1 << 3);  // ELSB=TPM2_C0SC[3]=0
-}
-
-
-void delay2() {
-	for (int i = 0; i < 200000; i++);
-}
-
 int main(void) {
+	//SystemCoreClockUpdate();
 	InitTPMPWM();
-	setupBeforeChangeCnv();
-	TPM1_REG->CONTROLS[0].CnV = DIREITA_90;
-
-	delay2();
-
-	setupBeforeChangeCnv();
-	TPM1_REG->CONTROLS[0].CnV = ORIGEM_0;
-
-	delay2();
+	//TPM1_REG->CONTROLS[0].CnV = 0x0000;  	// 0% duty cycle
 
 
-	setupBeforeChangeCnv();
-	TPM1_REG->CONTROLS[0].CnV = DIREITA_180;
+	// tempo ON
+//	TPM2_REG->CONTROLS[0].CnV = (float)0x0666 * 0.5;  	// 0x0666 = 1638 (metade de 3276) --> 50% duty cycle
+	TPM2_REG->CONTROLS[0].CnV =  327;
+	TPM1_REG->CONTROLS[0].CnV =  2620;
 
-	delay2();
+	//TPM1_REG->CONTROLS[0].CnV = 0x0EA6;  	// 0x0EA6 = 3750 (metade de 7500) --> 50% duty cycle
+
+	//TPM1_REG->CONTROLS[0].CnV = 0x0753;  	// 0x0EA6
 
 	while(1){
-		steps();
-	}
 
+	}
 
     return 0 ;
 }
